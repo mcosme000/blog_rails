bfb331e700e6396157449ddb071f7b1b90dd736a
Maria Cosmen <mcosme00@gmail.com>
2023-01-04 16:50:26 +0900

    Add form to create and save new articles, check validations and display error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+      # So I can use @article in new because I'm rendering
+      # it from the create method!
+    end
+  end
 end
