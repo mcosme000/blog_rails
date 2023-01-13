c6d534fa5aa806550ec8784921c35c93095c7789
Maria Cosmen <mcosme00@gmail.com>
2023-01-13 20:31:47 +0900

    EDIT action and SHOW view for each user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @colors = ['#0476f1', '#cbbada', '#e7e5e6', '#f4c524']
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Hey #{@user.username} your account was successfully updated"
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
