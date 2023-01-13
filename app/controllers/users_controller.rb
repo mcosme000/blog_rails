fdd6695d8f4ab6ddb804e8b340d023cafdaf8b14
Maria Cosmen <mcosme00@gmail.com>
2023-01-13 16:32:16 +0900

    NEW and CREATE actions and signup form for new users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to MOSHI blog, #{@user.username}, you have signup successfully"
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password_digest)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   root to: 'pages#home'
   get 'about', to: 'pages#about'
 
   # my articles routes are all here:
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
