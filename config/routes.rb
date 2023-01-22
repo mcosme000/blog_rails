6f50046e5e6cd0fcfe4df6ebc4519553f8ee1671
Maria Cosmen <mcosme00@gmail.com>
2023-01-22 16:10:07 +0900

    Login and logout actions with restrictions in homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users do
-    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session_path
+    get '/users/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session_path
   end
 
   root to: 'pages#home'
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
   # my articles routes are all here:
   resources :articles
   resources :users, except: [:new]
-
 end
